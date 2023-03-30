38e4312864777c167b97c9ba1724901dbb2cc174
SolanaG <gomez.solanarocio@gmail.com>
2023-03-30 12:56:26 -0300

    work-style

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./work.css";
 import IMG1 from "../../assets/memes.png";
+import IMG2 from "../../assets/magicspoon.png";
+import IMG3 from "../../assets/verdevolver.png";
 
 const Work = () => {
   return (
@@ -12,48 +14,75 @@ const Work = () => {
             <img src={IMG1} alt="Memes" />
           </div>
           <h3>Generador de Memes</h3>
-          <a href="https://github.com/SolanaG" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://dribbble.com/Alien_pixels"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
-        </article>
-        <article className="work__item">
-          <div className="work__item-img">
-            <img src={IMG1} alt="VerdeVolver" />
+          <h5>Teamwork</h5>
+          <div className="work__item-cta">
+            <a
+              href="https://github.com/Lupe-Accareddu/Editor-de-Memes"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://lupe-accareddu.github.io/Editor-de-Memes/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
           </div>
-          <h3>VerdeVolver</h3>
-          <a href="https://github.com/SolanaG" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://dribbble.com/Alien_pixels"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
         </article>
         <article className="work__item">
           <div className="work__item-img">
-            <img src={IMG1} alt="Magic Spoon" />
+            <img src={IMG2} alt="MagicSpoon" />
           </div>
           <h3>Magic Spoon</h3>
-          <a href="https://github.com/SolanaG" className="btn" target="_blank">
-            Github
-          </a>
-          <a
-            href="https://dribbble.com/Alien_pixels"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live Demo
-          </a>
+          <h5>Personal work</h5>
+          <div className="work__item-cta">
+            <a
+              href="https://github.com/SolanaG/Food-PI"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com/SolanaG/Food-PI"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="work__item">
+          <div className="work__item-img">
+            <img src={IMG3} alt="VerdeVolver" />
+          </div>
+          <h3>VerdeVolver</h3>
+          <h5>Teamwork</h5>
+          <div className="work__item-cta">
+            <a
+              href="https://github.com/natalia-iglesias/VerDeVolver---PF"
+              className="btn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+            <a
+              href="https://ver-de-volver-pf.vercel.app/"
+              className="btn btn-primary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Live Demo
+            </a>
+          </div>
         </article>
       </div>
     </section>
