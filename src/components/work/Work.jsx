9efb4abae84429fbead88ea0900a2b3d3ade8558
SolanaG <gomez.solanarocio@gmail.com>
2023-03-30 15:29:24 -0300

    data-array

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -4,86 +4,67 @@ import IMG1 from "../../assets/memes.png";
 import IMG2 from "../../assets/magicspoon.png";
 import IMG3 from "../../assets/verdevolver.png";
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Editor de Memes",
+    work: "Teamwork",
+    github: "https://github.com/Lupe-Accareddu/Editor-de-Memes",
+    demo: "https://lupe-accareddu.github.io/Editor-de-Memes/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Magic Spoon",
+    work: "Personal work",
+    github: "https://github.com/SolanaG/Food-PI",
+    demo: "https://github.com/SolanaG/Food-PI",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "VerdeVolver",
+    work: "Teamwork",
+    github: "https://github.com/natalia-iglesias/VerDeVolver---PF",
+    demo: "https://ver-de-volver-pf.vercel.app/",
+  },
+];
+
 const Work = () => {
   return (
     <section id="work">
       <h2>My Work</h2>
       <div className="container work__container">
-        <article className="work__item">
-          <div className="work__item-img">
-            <img src={IMG1} alt="Memes" />
-          </div>
-          <h3>Generador de Memes</h3>
-          <h5>Teamwork</h5>
-          <div className="work__item-cta">
-            <a
-              href="https://github.com/Lupe-Accareddu/Editor-de-Memes"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://lupe-accareddu.github.io/Editor-de-Memes/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="work__item">
-          <div className="work__item-img">
-            <img src={IMG2} alt="MagicSpoon" />
-          </div>
-          <h3>Magic Spoon</h3>
-          <h5>Personal work</h5>
-          <div className="work__item-cta">
-            <a
-              href="https://github.com/SolanaG/Food-PI"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://github.com/SolanaG/Food-PI"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="work__item">
-          <div className="work__item-img">
-            <img src={IMG3} alt="VerdeVolver" />
-          </div>
-          <h3>VerdeVolver</h3>
-          <h5>Teamwork</h5>
-          <div className="work__item-cta">
-            <a
-              href="https://github.com/natalia-iglesias/VerDeVolver---PF"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <a
-              href="https://ver-de-volver-pf.vercel.app/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, work, github, demo }) => {
+          return (
+            <article key={id} className="work__item">
+              <div className="work__item-img">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <h5>{work}</h5>
+              <div className="work__item-cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <a
+                  href={demo}
+                  className="btn btn-primary"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
